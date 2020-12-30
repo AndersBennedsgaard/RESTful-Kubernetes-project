c702a4a3fd256421f0e9379af79608e1947c019e
Anders Bennedsgaard <abbennedsgaard@gmail.com>
2020-12-30 13:25:23 +0100

    Improved the logger

--- app/logger.py
+++ app/logger.py
@@ -1,9 +1,31 @@
 import logging as log
+from logging.config import dictConfig
 
 
-# Logging
-log.basicConfig(
-    filename="api.log",
-    level=log.DEBUG,
-    format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "precise": {
+                "format": "%(asctime)s | %(levelname)s | %(name)s | %(module)s | %(message)s"
+            },
+            "brief": {"format": "%(asctime)s | %(levelname)s | %(message)s"},
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "brief",
+                "level": "WARNING",
+            },
+            "file": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "formatter": "precise",
+                "filename": "api.log",
+                "maxBytes": 1024 ** 2,
+                "backupCount": 1,
+            },
+        },
+        "root": {"level": "DEBUG", "handlers": ["console", "file"]},
+    }
 )
